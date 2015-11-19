1166f8807cf966814c7a3fdd9a0e0aeaf023c309
Tiago de Oliveira <tiago.oliveira@gmx.us>
2015-11-18 22:56:03 -0200

    Adding FunSpec and improving repots.

--- src/main/scala/io/tiagodeoliveira/Game.scala
+++ src/main/scala/io/tiagodeoliveira/Game.scala
@@ -6,8 +6,8 @@ import org.json4s.native.JsonMethods
 import scala.collection.mutable.ArrayBuffer
 
 /**
-  * Created by tiagooliveira on 11/18/15.
-  */
+ * Created by tiagooliveira on 11/18/15.
+ */
 case class Player(name: String)
 
 case class Weapon(name: String)
@@ -16,17 +16,37 @@ case class Kill(killer: Player, killed: Player, weapon: Weapon)
 
 class Game(val id: Int, var totalKills: Int, val kills: ArrayBuffer[Kill]) {
 
+  private val MASTER_PLAYER = "<world>"
+
   override def toString = {
+    JsonMethods.compact(this.getGameAsJson())
+  }
+
+  def simpleReport() = {
+    val jsonGame = this.getGameAsJson()
+    jsonGame.::("players")
+  }
+
+  private def getGameAsJson() = {
+    val validKills = this.getOnlyValidKills
+
     val json =
       (s"game_$id" ->
         ("total_kills" -> totalKills) ~
-          ("players" -> kills.groupBy(_.killer.name).map(_._1)) ~
-          ("kills" -> kills.groupBy(_.killer.name).map { kill =>
-            ((kill._1) -> kill._2.size)
+          ("players" -> validKills.map(_._1)) ~
+          ("kills" -> validKills.map { kill =>
+            ((kill._1) -> getKillsByPlayer(kill._1))
           })
         )
 
-    JsonMethods.compact(JsonMethods.render(json))
+    JsonMethods.render(json)
   }
 
+  private def getOnlyValidKills = {
+    kills.groupBy(_.killer.name).filter(_._1 != MASTER_PLAYER)
+  }
+
+  private def getKillsByPlayer(player: String) = {
+    kills.count(kill => kill.killer.name == MASTER_PLAYER && kill.killed.name == player)
+  }
 }
--- src/test/scala/io/tiagodeoliveira/LogParserSpec.scala
+++ src/test/scala/io/tiagodeoliveira/LogParserSpec.scala
@@ -1,57 +1,70 @@
 package io.tiagodeoliveira
 
-import org.scalatest.{BeforeAndAfter, FunSuite}
+import org.scalatest.{BeforeAndAfter, FunSpec}
 
 /**
-  * Created by tiagooliveira on 11/17/15.
-  */
-class LogParserSpec extends FunSuite with BeforeAndAfter {
+ * Created by tiagooliveira on 11/17/15.
+ */
+class LogParserSpec extends FunSpec with BeforeAndAfter {
 
   var logParser: LogParser = _
-  var logFileName = "src/test/resources/games.log"
+  var singleGameFileName = "src/test/resources/single_game.log"
+  var allGamesFileName = "src/test/resources/all_games.log"
 
   before {
     logParser = new LogParser
   }
 
-  test("If the method returns an game array") {
-    val result = logParser.parseFile(logFileName)
-    assert(result != null)
+  describe("A set of tests with only one game") {
+    it("should return a valid result array") {
+      val result = logParser.parseFile(singleGameFileName)
+      assert(result != null)
+    }
+
+    it("shoulld return the correct amount of game kills history") {
+      val expectedGamesAmount = 131
+
+      val result = logParser.parseFile(singleGameFileName)
+      assertResult(expectedGamesAmount)(result.last.kills.size)
+    }
+
+    it("should return the correct game kills history for a given game") {
+      val result = logParser.parseFile(singleGameFileName)
+      assertResult(new Kill(new Player("Dono da Bola"), new Player("Isgalamido"), new Weapon("MOD_ROCKET")))(result.last.kills.head)
+    }
+
+    it(" should return the game as the output json") {
+      val expectedJsonOutput = """{"game_1":{"total_kills":131,"players":["Dono da Bola","Assasinu Credi","Oootsimo","Zeh","Mal","Isgalamido"],"kills":{"Dono da Bola":2,"Assasinu Credi":3,"Oootsimo":2,"Zeh":2,"Mal":6,"Isgalamido":2}}}"""
+
+      val result = logParser.parseFile(singleGameFileName)
+      assertResult(expectedJsonOutput)(result.last.toString)
+    }
   }
 
-  test("parser returns the correct amount of games from the log file") {
-    val expectedGamesAmount = 21
+  describe("A set of tests with many games on a file") {
+    it("should return the right amount of games on the log") {
+      val expectedGamesAmount = 21
 
-    val result = logParser.parseFile(logFileName)
-    assertResult(expectedGamesAmount)(result.size)
-  }
+      val result = logParser.parseFile(allGamesFileName)
+      assertResult(expectedGamesAmount)(result.size)
+    }
 
-  test("returns the correct amount of kills per game") {
-    val expectedGamesKillCounts = Map(1 -> 0, 2 -> 11, 3 -> 4, 4 -> 105, 5 -> 14, 19 -> 95, 20 -> 3, 21 -> 131)
+    it("it should return the correct kill amounts from the given games") {
+      val expectedGamesKillCounts = Map(1 -> 0, 2 -> 11, 3 -> 4, 4 -> 105, 5 -> 14, 19 -> 95, 20 -> 3, 21 -> 131)
 
-    val result = logParser.parseFile(logFileName)
-    result.foreach { game =>
-      if (expectedGamesKillCounts contains game.id) {
-        assertResult(expectedGamesKillCounts(game.id))(game.totalKills)
+      val result = logParser.parseFile(allGamesFileName)
+      result.foreach { game =>
+        if (expectedGamesKillCounts contains game.id) {
+          assertResult(expectedGamesKillCounts(game.id))(game.totalKills)
+        }
       }
     }
   }
 
-  test("return the correct amount of game kills history") {
-    val expectedGamesAmount = 131
-
-    val result = logParser.parseFile(logFileName)
-    assertResult(expectedGamesAmount)(result.last.kills.size)
+  describe("Test game reports from a single match") {
+    it("should return a simple game report") {
+      val result = logParser.parseFile(singleGameFileName)
+      result.head.simpleReport()
+    }
   }
-
-  test("return the correct game kills history for a given game") {
-    val result = logParser.parseFile(logFileName)
-    assertResult(new Kill(new Player("Dono da Bola"), new Player("Isgalamido"), new Weapon("MOD_ROCKET")))(result.last.kills.head)
-  }
-
-  test("game list to string") {
-    val result = logParser.parseFile(logFileName)
-    assertResult("")(result.last.toString)
-  }
-
 }
